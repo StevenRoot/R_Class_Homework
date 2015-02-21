624e08ec850c7cd447a8399fe9fe09f61b62617f
Steven Root <stevenroot@Stevens-iMac.local>
2015-02-21 15:42:00 -0500

    updated best  - still in process

--- best.R
+++ best.R
@@ -1,7 +1,33 @@
 best <- function(state, outcome) {
   ## Read outcome data
+  outcome_data <- read.csv("rprog-data-ProgAssignment3-data/outcome-of-care-measures.csv")
+  outcome_prefix <- "Hospital.30.Day.Death..Mortality..Rates.from."
+  
   ## Check that state and outcome are valid
-  ## Return hospital name in that state with lowest 30-day death
-  ## rate
+  outcome_words <- strsplit(outcome," ")[[1]]
+  outcome_append_text <- paste(toupper(substring(outcome_words,1,1)), substring(outcome_words,2), sep = "", collapse = ".")
+  final_outcome <- paste(outcome_prefix, outcome_append_text, sep="")
+  
+  outcome_valid <- final_outcome %in% colnames(outcome_data)
+  state_valid <- state %in% outcome_data[, 7]
+  
+  # valid if state is int he state column and malady s in the column names....
+  if ((outcome_valid == TRUE) & (state_valid == TRUE))
+  {
+    ## Return hospital name in that state with lowest 30-day death
+    ## rate
+    
+  }
+  else if (!outcome_valid)
+  {
+    stop("invalid outcome")
+  }
+  else # (!state_valid)
+  {
+    stop("invalid state")
+  } 
 }
 
+#"Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
+#"Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"
+#"Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia" 
--- tests/hw3_1.R
+++ tests/hw3_1.R
@@ -1,14 +1,101 @@
 test.hw1 <- function()
 {
-  checkEquals(1,1)
-  #4.064, round(pollutantmean(file.path(getwd(),"specdata"), "sulfate", 1:10),3))
+  expected_result <- "CYPRESS FAIRBANKS MEDICAL CENTER"
+  checkEquals(expected_result, best("TX","heart attack"))
+}
+
+test.hw5 <- function()
+{
+  #expected_result <- "Error in best(""BB", ""heart attack""") : invalid state"
+  expected_result <- "invalid state"
+  checkException(best("BB", "heart attack"), expected_result)
+}
+
+test.hw6 <- function()
+{
+  #expected_result <- "Error in best("NY", "hert attack") : invalid outcome"
+  expected_result <- "invalid outcome"
+  
+  checkException(best("NY", "hert attack"), expected_result)
 }
 
 
 
+test.deactivation <- function()
+{
+  DEACTIVATED('Deactivating this test function')
+}
+
 if (FALSE)
 {  
-test.hw4<- function()
+  
+  stop_test <- function(){stop("invalid state")} 
+  dont_stop_test <- function(){"don't stop"}
+  
+  test.hw0 <- function()
+  {
+    expected_result <- "00Error in stop_test() : invalid state"
+    checkException(stop_test(), expected_result)
+  }
+  
+  test.hw01 <- function()
+  {
+    expected_result <- "00Error in stop_test() : invalid state"
+    checkException(dont_stop_test(), expected_result)
+  }
+  
+  
+  test.hw2 <- function()
+  {
+    expected_result <- "FORT DUNCAN MEDICAL CENTER"
+    checkEquals(expected_result, best("TX", "heart failure"))
+  }
+  test.hw3 <- function()
+  {
+    expected_result <- "JOHNS HOPKINS HOSPITAL, THE"
+    checkEquals(expected_result, best("MD", "heart attack"))
+  }
+  
+  test.hw4 <- function()
+  {
+    expected_result <- "GREATER BALTIMORE MEDICAL CENTER"
+    checkEquals(expected_result, best("MD", "pneumonia"))
+  }
+  
+  test.hw5 <- function()
+  {
+    #expected_result <- "Error in best(""BB", ""heart attack""") : invalid state"
+    expected_result <- "invalid state"
+    checkException(best("BB", "heart attack"), expected_result)
+  }
+  
+  test.hw6 <- function()
+  {
+    #expected_result <- "Error in best("NY", "hert attack") : invalid outcome"
+    expected_result <- "invalid outcome"
+    
+    checkException(best("NY", "hert attack"), expected_result)
+  }
+
+  test.hw7 <- function()
+  {
+    expected_result <- "DETAR HOSPITAL NAVARRO"
+    checkEquals(expected_result, rankhospital("TX", "heart failure", 4))
+  }
+  
+  test.hw8 <- function()
+  {
+    expected_result <- "HARFORD MEMORIAL HOSPITAL"
+    checkEquals(expected_result, rankhospital("MD", "heart attack", "worst"))
+  }
+
+  test.hw9 <- function()
+  {
+    expected_result <- NA
+    checkEquals(expected_result, rankhospital("MN", "heart attack", 5000))
+  }
+  
+  test.hw4<- function()
 {
   # checkEquals(6, factorial(3))
   # checkEqualsNumeric(6, factorial(3))
@@ -17,9 +104,4 @@ test.hw4<- function()
   # checkTrue(2 + 2 == 4, 'Arithmetic works')
   # checkException(log('a'), 'Unable to take the log() of a string')
 }
-}
-
-test.deactivation <- function()
-{
-  DEACTIVATED('Deactivating this test function')
 }
